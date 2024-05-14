25c5790ab92face00d58974ec08ac03af1c6c348
skpark33 <skpark@sqisoft.com>
2024-05-14 18:56:15 +0900

    12334

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2445359173"
+    serviceWorkerVersion: "4072612215"
   }
 });
